446c7a1f0f3b643fb2287e2277ca7bf3e076c238
Puneeth Kumar <puneeth.kumar@zensar.com>
2021-01-26 09:06:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileWriteAndRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream f = new FileStream(@"result.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (StreamWriter sw=new StreamWriter(f))
+                {
+                    string str;
+                    Console.WriteLine("enter the content to write a file, enter ? to quit: ");
+                    for (; ; )
+                    {
+                        str = Console.ReadLine();
+                        if (str == "?")
+                        {
+                            break;
+                        }
+                        else{
+                            sw.WriteLine(str); 
+                        }
+                    }
+                }
+                using (StreamReader sr=new StreamReader(@"result.txt"))
+                {
+                    Console.WriteLine("the content of the file is: ");
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
