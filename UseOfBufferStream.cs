3e1c795bc551639867ea7c7f7515f10bc1f2a4c9
Puneeth Kumar <puneeth.kumar@zensar.com>
2021-01-26 22:35:54 +0530

    use of MemoryStream and BufferedStream

---
+++ UseOfBufferStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+namespace FileWriteAndRead
+{
+    class UseOfBufferStream
+    {
+        static void Main()
+        {
+            MemoryStream ms = new MemoryStream();
+            BufferedStream b = new BufferedStream(ms);
+            b.WriteByte(1);
+            byte[] a = { 1, 2, 3 };
+            b.Write(a, 0, a.Length);
+            byte[] a1 = new byte[252];
+            ms.Write(a1, 0, a1.Length);
+            ms.WriteByte(4);
+            
+            
+        }
+    }
+}
---
+++ UseOfMemoryStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileWriteAndRead
+{
+    class UseOfMemoryStream
+    {
+        static void Main()
+        {
+            MemoryStream ms = new MemoryStream();
+            ms.WriteByte(7);
+            byte[] b = {1,2,3 };
+            ms.Write(b,0,b.Length);
+            byte[] b1 = new byte[252];
+            ms.Write(b1,0,b1.Length);
+            ms.WriteByte(4);
+            MemoryStream ms1 = new MemoryStream(2);
+            ms1.WriteByte(1);
+            ms1.WriteByte(2);
+            ms1.WriteByte(3);
+            MemoryStream ms2 = new MemoryStream(b,false);
+            //ms2.WriteByte(3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
